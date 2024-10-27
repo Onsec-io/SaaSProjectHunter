ee028f285471296efd2ffbce24ec040570a488d4
noname <noname@localhost>
2024-10-28 00:48:44 +0100

    Fix Apkpure

--- modules/Apkpure.py
+++ modules/Apkpure.py
@@ -34,7 +34,15 @@ def run(words):
     urls += compile_url('apkpure.net/developer/', [item.upper() for item in words])
     log.debug('Run requests...')
     loop = asyncio.get_event_loop()
-    responses = loop.run_until_complete(async_requests(urls, method='GET', http2=True))
-    founded_projects = [str(r.url) for r in responses if r.status_code == 200 and ('<div class="list-title">' in r.text or '<div class="first first-apk' in r.text)]
+    responses = loop.run_until_complete(async_requests(urls, method='GET', http2=False))
+    founded_projects = []
+    for r in responses:
+        if r.status_code == 200 and (('<span>All ' in r.text and ' Apps</span>' in r.text) or ' search results for Apps' in r.text):
+            founded_projects.append(str(r.url))
+        # elif r.status_code == 200 and ('<span>0</span> search results found' in r.text):
+        #     pass
+        # elif r.status_code != 404:
+        #     print(r.status_code, r.url)
+
     log.info('{}: founded {} sites'.format(get_name(), len(founded_projects)))
     return founded_projects
