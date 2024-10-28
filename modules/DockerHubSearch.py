2dfe06d1c6c2a03b10920864871d4977b53e1ac0
noname <noname@localhost>
2024-10-28 16:33:46 +0100

    Fix DockerHubSearch

--- modules/DockerHubSearch.py
+++ modules/DockerHubSearch.py
@@ -29,14 +29,14 @@ def wordslist_for_check_module():
 
 def run(words):
     log.debug('Checking the wordlist for requirements of {} module...'.format(get_name()))
-    urls = compile_url('hub.docker.com/api/content/v1/products/search?page_size=1&q=', words)
+    urls = compile_url('hub.docker.com/api/search/v3/catalog/search?size=0&query=', words)
     log.debug('Run requests...')
     loop = asyncio.get_event_loop()
     responses = loop.run_until_complete(async_requests(urls, method='get'))
     founded_projects = []
     for r in responses:
-        if r.status_code == 200 and r.json()['count'] != 0:
-            word_from_url = str(r.url).split('q=')[1]
+        if r.status_code == 200 and r.json()['total'] != 0:
+            word_from_url = str(r.url).split('query=')[1]
             founded_projects.append('https://hub.docker.com/search?q={}'.format(word_from_url))
     log.info('{}: founded {} sites'.format(get_name(), len(founded_projects)))
     return founded_projects
