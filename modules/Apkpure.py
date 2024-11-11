81509c95cd25065460e0b34fd9cbd0739ba68e8f
noname <noname@localhost>
2024-11-11 23:37:28 +0100

    Update Apkpure

--- modules/Apkpure.py
+++ modules/Apkpure.py
@@ -9,7 +9,7 @@ def get_name():
 
 
 def get_version():
-    return '1.0'
+    return '1.1'
 
 
 def get_tags():
@@ -30,19 +30,15 @@ def wordslist_for_check_module():
 def run(words):
     log.debug('Checking the wordlist for requirements of {} module...'.format(get_name()))
     words = list(set([item.lower() for item in words]))  # lowercase and remove doubles
-    urls = compile_url('apkpure.net/search?q=', [f'%22{item.lower()}%22' for item in words])
+    urls = compile_url('apkpure.net/api/v1/search_suggestion_new?limit=1&key=', [f'{item.lower()}' for item in words])
     urls += compile_url('apkpure.net/developer/', [item.upper() for item in words])
     log.debug('Run requests...')
     loop = asyncio.get_event_loop()
-    responses = loop.run_until_complete(async_requests(urls, method='GET', http2=False))
+    responses = loop.run_until_complete(async_requests(urls, method='HEAD', http2=False))
     founded_projects = []
     for r in responses:
-        if r.status_code == 200 and (('<span>All ' in r.text and ' Apps</span>' in r.text) or ' search results for Apps' in r.text):
+        if (r.status_code == 200 and not r.headers.get('Content-Length', None)) or (r.headers.get('Content-Length') == 2):
             founded_projects.append(str(r.url))
-        # elif r.status_code == 200 and ('<span>0</span> search results found' in r.text):
-        #     pass
-        # elif r.status_code != 404:
-        #     print(r.status_code, r.url)
 
     log.info('{}: founded {} sites'.format(get_name(), len(founded_projects)))
     return founded_projects
